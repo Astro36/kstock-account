37919969482feb6b76e265df617ef90e2d12cc3c
Seungjae Park <astro.psj@gmail.com>
2024-06-20 19:38:06 +0900

    Initial commit

---
+++ stock_account/mirae.py
@@ -0,0 +1,121 @@
+import requests
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+
+class MiraeAccount:
+    def login(user_id: str, user_password: str):
+        options = Options()
+        options.add_argument("--headless=new")
+
+        driver = webdriver.Edge(options=options, service=Service(EdgeChromiumDriverManager().install()))
+        driver.get("https://securities.miraeasset.com/mw/login.do")
+
+        driver.execute_script(f"document.querySelector('#usid').value = '{user_id}'")
+        driver.execute_script(f"document.querySelector('#clt_ecp_pwd').value = '{user_password}'")
+        driver.execute_script("doSubmit();")
+        _ = WebDriverWait(driver, 10).until(lambda x: x.current_url == "https://securities.miraeasset.com/mw/main.do")
+
+        access_token = driver.get_cookie("MIREADW_D")["value"]
+        driver.close()
+
+        return MiraeAccount(access_token)
+
+    def __init__(self, access_token) -> None:
+        self.access_token = access_token
+
+    def get_assets(self):
+        r = requests.post(
+            "https://securities.miraeasset.com/hkd/hkd1003/a03.json",
+            data={"pd_tcd": "01"},
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+            cookies={"MIREADW_D": self.access_token},
+        )
+        equities = [
+            {
+                "account_number": int(row["admn_acno"]),
+                "symbol": row["itm_no"],
+                "name": row["itm_nm1"],
+                "currency": row["curr_cd"],
+                "market_value": float(row["pitm_ea"]),
+                "market_value_krw": int(row["ea"]),
+                "entry_value": float(row["pchs_a1"]),
+                "entry_value_krw": int(row["pchs_a"]),
+                "unrealized_pl": float(row["itm_ev_pl"]),
+                "unrealized_pl_krw": int(row["ev_pl_a"]),
+                "unrealized_pl_percent": float(row["ev_pl_r"]),
+                "quantity": float(row["hldg_q"]),
+                "market_price": float(row["crpr"]),
+                "entry_price": float(row["pchs_a1"]) / float(row["hldg_q"]),
+            }
+            for row in r.json()["grid01"]
+        ]
+
+        r = requests.post(
+            "https://securities.miraeasset.com/hkd/hkd1003/a03.json",
+            data={"pd_tcd": "04"},
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+            cookies={"MIREADW_D": self.access_token},
+        )
+        golds = [
+            {
+                "account_number": int(row["admn_acno"]),
+                "symbol": row["itm_no"],
+                "name": row["itm_nm1"],
+                "currency": row["curr_cd"],
+                "market_value": float(row["pitm_ea"]),
+                "market_value_krw": int(row["ea"]),
+                "entry_value": float(row["pchs_a1"]),
+                "entry_value_krw": int(row["pchs_a"]),
+                "unrealized_pl": float(row["itm_ev_pl"]),
+                "unrealized_pl_krw": int(row["ev_pl_a"]),
+                "unrealized_pl_percent": float(row["ev_pl_r"]),
+                "quantity": float(row["hldg_q"]),
+                "market_price": float(row["crpr"]),
+                "entry_price": float(row["pchs_a1"]) / float(row["hldg_q"]),
+            }
+            for row in r.json()["grid01"]
+        ]
+
+        r = requests.post(
+            "https://securities.miraeasset.com/hkd/hkd1003/a05.json",
+            cookies={"MIREADW_D": self.access_token},
+        )
+        cash_equivalents = [
+            {
+                "account_number": int(row["acno"]),
+                "name": row["rp_pd_nm"],
+                "currency": row["curr_cd"],
+                "market_value": float(row["frc_ea"]),
+                "market_value_krw": int(row["ea"]),
+                "entry_value": float(row["frc_rp_ctrt_a"]),
+                "entry_value_krw": int(float(row["cony_buy_a"])),
+                "unrealized_pl": float(row["frc_ev_pl_a"]),
+                "unrealized_pl_krw": int(row["ev_pl_a"]),
+                "unrealized_pl_percent": float(row["frc_ev_pl_r"]),
+            }
+            for row in r.json()["grid01"]
+        ]
+
+        r = requests.post(
+            "https://securities.miraeasset.com/hkd/hkd1003/a11.json",
+            data={"qry_tcd": "1"},
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+            cookies={"MIREADW_D": self.access_token},
+        )
+        foreign_currencies = [
+            {
+                "account_number": int(row["acno"]),
+                "name": row["curr_cd"],
+                "currency": row["curr_cd"],
+                "market_value": float(row["mnyo_abl_a"]) / float(row["bas_exr"]),
+                "market_value_krw": int(float(row["mnyo_abl_a"])),
+                "exchange_rate": float(row["bas_exr"]),
+            }
+            for row in r.json()["GRID01"]
+        ]
+
+        return equities + golds + cash_equivalents + foreign_currencies
